90ae5de2ee4a824192808f55d91447590dd5d67a
Yunzhi Gao <gaoyunzhi@gmail.com>
2016-02-17 10:35:03 -0800

    commit

--- get_notes.py
+++ get_notes.py
@@ -1,4 +1,4 @@
-import httplib2
+import urllib2, time, random
 
 DOUBAN_HEADER = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
@@ -11,18 +11,42 @@ class DoubanFetcher():
 
 	def get_notes(self):
 		self.book_id = self.get_book_id()
+		self.note_link = 'http://book.douban.com/subject/' + \
+		 	self.book_id + '/annotation?sort=rank&start='
+		for i in xrange(0, 10):
+			start = str(10 * i)
+			notes = self.get_note_from_page(self.note_link + start)
+			for note in notes:
+				yield note
+			if len(notes) < 10:
+				break
 
 	def get_book_id(self):
 		url = 'http://www.douban.com/search?cat=1001&q=' + self.book_name
-		req = httplib2.Request(url, None, DOUBAN_HEADER)	
+		req = urllib2.Request(url, None, DOUBAN_HEADER)	
 		response = urllib2.urlopen(req).read()
 		start_index = response.find('book.douban.com%2Fsubject')
-		content = response[start_index:]
+		content = response[start_index + 28:]
 		end_index = content.find('%2F')
-		print content[:end_index]
 		return content[:end_index]
 
+	def get_note_from_page(self, link):
+		time.sleep(random.randint(1, 5))
+		req = urllib2.Request(link, None, DOUBAN_HEADER)
+		response = urllib2.urlopen(req).read()
+		anchor = 'div class="reading-note"'
+		anchor_index = response.find(anchor)
+		while anchor_index != -1:
+			text_anchor = '<div class="all hidden" style="display:none" >'
+			start_index = response.find(text_anchor, anchor_index) + len(text_anchor)
+			text_end_anchor = '<div class="col-rec-con clearfix">'
+			end_index = response.find(text_end_anchor, start_index)
+			yeild response[start_index: end_index]
+			anchor_index = response.find(anchor, end_index)
+
 def get_notes(book_name):
-	douban_fetcher = new DoubanFetcher(book_name)
-	douban_fetcher.get_notes()
-	return []
+	douban_fetcher = DoubanFetcher(book_name)
+	notes = douban_fetcher.get_notes()
+	for note in notes:
+		print note
+	return notes
