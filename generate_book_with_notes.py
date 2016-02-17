93ffb651a60905bdf263470172df89120558a1f4
Yunzhi Gao <gaoyunzhi@gmail.com>
2016-02-17 09:55:57 -0800

    commit

--- generate_book_with_notes.py
+++ generate_book_with_notes.py
@@ -10,24 +10,25 @@ book_dir, org_book_file = os.path.split(org_path)
 book_name = os.path.splitext(org_book_file)[0]
 htmlz_path = os.path.join(book_dir, book_name + '.htmlz')
 tmp_dir = os.path.join(book_dir, book_name + "_tmp")
-# subprocess.call(["rm", htmlz_path], stdout=FNULL, stderr=FNULL)
-# subprocess.call([os.path.join(calibre_dir, "ebook-convert"), org_path, htmlz_path])
-subprocess.call(["rm", "-rf", tmp_dir], stdout=FNULL, stderr=FNULL)
-subprocess.call(["unzip", htmlz_path, "-d", tmp_dir], stdout=FNULL)
+# # subprocess.call(["rm", htmlz_path], stdout=FNULL, stderr=FNULL)
+# # subprocess.call([os.path.join(calibre_dir, "ebook-convert"), org_path, htmlz_path])
+# subprocess.call(["rm", "-rf", tmp_dir], stdout=FNULL, stderr=FNULL)
+# subprocess.call(["unzip", htmlz_path, "-d", tmp_dir], stdout=FNULL)
 
 # now deal with the html format
 notes = get_notes(book_name)
 insert_notes(notes, tmp_dir)
-new_htmlz_path = os.path.join(tmp_dir, book_name + '_with_notes.htmlz')
-subprocess.call(["rm", new_htmlz_path], stdout=FNULL, stderr=FNULL)
-print new_htmlz_path
-os.system(subprocess.list2cmdline(["cd", tmp_dir, "&&",
-	"zip", "-r", new_htmlz_path, "*"]))
-new_mobi_path = os.path.join(book_dir, book_name + '_with_notes.mobi')
-subprocess.call(["rm", new_mobi_path], stdout=FNULL, stderr=FNULL)
-subprocess.call(
-	[os.path.join(calibre_dir, "ebook-convert"), new_htmlz_path, new_mobi_path])
-subprocess.call(["rm", htmlz_path], stdout=FNULL, stderr=FNULL)
-# subprocess.call(["rm", "-rf", tmp_dir], stdout=FNULL, stderr=FNULL)
+
+# export
+# new_htmlz_path = os.path.join(tmp_dir, book_name + '_with_notes.htmlz')
+# subprocess.call(["rm", new_htmlz_path], stdout=FNULL, stderr=FNULL)
+# os.system(subprocess.list2cmdline(["cd", tmp_dir, "&&",
+# 	"zip", "-r", new_htmlz_path, "*"]))
+# new_mobi_path = os.path.join(book_dir, book_name + '_with_notes.mobi')
+# subprocess.call(["rm", new_mobi_path], stdout=FNULL, stderr=FNULL)
+# subprocess.call(
+# 	[os.path.join(calibre_dir, "ebook-convert"), new_htmlz_path, new_mobi_path])
+# subprocess.call(["rm", htmlz_path], stdout=FNULL, stderr=FNULL)
+# # subprocess.call(["rm", "-rf", tmp_dir], stdout=FNULL, stderr=FNULL)
 
 
--- get_notes.py
+++ get_notes.py
@@ -1,2 +1,28 @@
+import httplib2
+
+DOUBAN_HEADER = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+  'Origin':'http://book.douban.com',
+};
+
+class DoubanFetcher():
+	def __init__(self, book_name):
+		self.book_name = book_name
+
+	def get_notes(self):
+		self.book_id = self.get_book_id()
+
+	def get_book_id(self):
+		url = 'http://www.douban.com/search?cat=1001&q=' + self.book_name
+		req = httplib2.Request(url, None, DOUBAN_HEADER)	
+		response = urllib2.urlopen(req).read()
+		start_index = response.find('book.douban.com%2Fsubject')
+		content = response[start_index:]
+		end_index = content.find('%2F')
+		print content[:end_index]
+		return content[:end_index]
+
 def get_notes(book_name):
+	douban_fetcher = new DoubanFetcher(book_name)
+	douban_fetcher.get_notes()
 	return []
